a42d1f6e90ddc12f83339e8e9d48a621aec8fe3f
ania-brol <117512754+anna-brol@users.noreply.github.com>
2023-03-09 16:42:47 +0100

    Add MathJax

--- script.js
+++ script.js
@@ -143,4 +143,173 @@ function kmod_listQ() {
 element_usability.onchange = kmod_listQ;
 element_durability.onchange = kmod_listQ;
 
+
 const element_fmd = document.getElementById("fmd");
+
+
+
+// var math = MathJax.tex2chtml("\\frac{1}{" + k + "}");
+// var math = MathJax.tex2chtml(
+//   "\\frac{\\tau_{Ed}} {\\displaystyle {\\tau_{Rd}}}= "
+// );
+
+// var math = MathJax.tex2chtml(
+//   "\\frac{\\tau_{Ed}}{\\displaystyle{\\tau_{Rd}}} = \\frac{" + tau + "}{\\displaystyle{" + tej + "}}"
+// );
+
+// var div = document.createElement("div");
+// div.appendChild(math);
+// document.body.appendChild(div);
+
+
+// fmd
+var a = MathJax.tex2chtml(
+  "f_{m,d}=\\frac{f_{m,k}*k_{mod}}{\\displaystyle{1.3}} = "
+);
+
+var div = document.createElement("div");
+div.appendChild(a);
+document.body.appendChild(div);
+
+// ft0d
+var b = MathJax.tex2chtml(
+  "f_{t,0,d}=\\frac{f_{t,0,k}*k_{mod}}{\\displaystyle{1.3}} = "
+);
+
+var div = document.createElement("div");
+div.appendChild(b);
+document.body.appendChild(div);
+
+// fvd
+var c = MathJax.tex2chtml(
+  "f_{v,d}=\\frac{f_{v,k}*k_{mod}}{\\displaystyle{1.3}} = "
+);
+
+var div = document.createElement("div");
+div.appendChild(c);
+document.body.appendChild(div);
+
+// fc90d
+var d = MathJax.tex2chtml(
+  "f_{c,90,d}=\\frac{f_{c,90,k}*k_{mod}}{\\displaystyle{1.3}} = "
+);
+
+var div = document.createElement("div");
+div.appendChild(d);
+document.body.appendChild(div);
+
+// fc0d
+var e = MathJax.tex2chtml(
+  "f_{c,0,d}=\\frac{f_{c,0,k}*k_{mod}}{\\displaystyle{1.3}} = "
+);
+
+var div = document.createElement("div");
+div.appendChild(e);
+document.body.appendChild(div);
+
+// fcAd
+var f = MathJax.tex2chtml(
+  "f_{c,\\alpha,d}=\\frac{f_{c,0,d}}{\\displaystyle\\frac{f_{c,0,d}}{k_{c,90}*f_{c,90,d}}*(\\sin(\\alpha))^2+(\\cos(\\alpha))^2} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(f);
+document.body.appendChild(div);
+
+// An
+var g = MathJax.tex2chtml(
+  "A_{n}=\\frac{N_{d}} {\\displaystyle f_{c,\\alpha,d}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(g);
+document.body.appendChild(div);
+
+// Sigma
+var h = MathJax.tex2chtml(
+  "\\sigma_{c,\\alpha,d}=\\frac{N_{d}*cos(\\alpha)} {\\displaystyle {b*s}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(h);
+document.body.appendChild(div);
+
+// Sigma_porownanie
+var i = MathJax.tex2chtml(
+  "\\frac{\\sigma_{c,\\alpha,d}} {\\displaystyle {f_{c,\\alpha,d}}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(i);
+document.body.appendChild(div);
+
+//l1v
+var j = MathJax.tex2chtml(
+  "l_{1,v}={s*tg(\\alpha)} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(j);
+document.body.appendChild(div);
+
+// Av
+var k = MathJax.tex2chtml(
+  "A_{v}=\\frac{N_{d}*cos(\\alpha)} {\\displaystyle {f_{v,d}}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(k);
+document.body.appendChild(div);
+
+// lv
+var l = MathJax.tex2chtml(
+  "l_{v}=\\frac{A_{v}} {\\displaystyle {b}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(l);
+document.body.appendChild(div);
+
+//l2v'
+var m = MathJax.tex2chtml(
+  "l_{2,v'}={l_{v}-l_{1,v}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(m);
+document.body.appendChild(div);
+
+//tauRd
+var n = MathJax.tex2chtml(
+  "\\tau_{Rd}=\\frac{N_{d}*cos(\\alpha)} {\\displaystyle {A_{v}}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(n);
+document.body.appendChild(div);
+
+//tauEd
+var o = MathJax.tex2chtml(
+  "\\tau_{Ed}=\\frac{N_{d}*cos(\\alpha)} {\\displaystyle{b*(l_{2,v}+l_{1,v})}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(o);
+document.body.appendChild(div);
+
+//tauEd/tauRd
+var o = MathJax.tex2chtml(
+  "\\frac{\\tau_{Ed}} {\\displaystyle {\\tau_{Rd}}} = "
+  );
+
+var div = document.createElement("div");
+div.appendChild(o);
+document.body.appendChild(div);
+
+
+
+
+
+
+
+
