526ecb62c599042c7e1756d19cac840ed4228d96
ania-brol <117512754+anna-brol@users.noreply.github.com>
2023-05-28 18:17:43 +0200

    Add angle and quantity values

--- screw.js
+++ screw.js
@@ -43,28 +43,28 @@ function calc_condition1(nd, rk){
   return nd / rk;
 }
 function calc_r1d(r1_screw, kmod, ym){
-  return 2 * 5 * r1_screw * kmod / ym;
+  return quantity2_value * r1_screw * kmod / ym;
 }
 function calc_condition2(nd, r1d){
   return nd / r1d;
 }
 function calc_a3c(diameter){
-  return 10 * diameter;
+  return quantity2_value * diameter;
 }
 function calc_a3t(diameter){
-  return ((10 + 5 * Math.cos(45*Math.PI/180)) * diameter);
+  return ((quantity2_value + quantity_value * Math.cos(alpha_value*Math.PI/180)) * diameter);
 }
 function calc_a4c(diameter){
-  return 5 * diameter;
+  return quantity_value * diameter;
 }
 function calc_a4t(diameter){
-  return ((5 + 5 * Math.sin(45*Math.PI/180)) * diameter);
+  return ((quantity_value + quantity_value * Math.sin(alpha_value*Math.PI/180)) * diameter);
 }
 function calc_a1(diameter){
-  return 0.7 * ((5 + 7 * Math.cos(45*Math.PI/180)) * diameter);
+  return 0.7 * ((quantity_value + 7 * Math.cos(alpha_value*Math.PI/180)) * diameter);
 }
 function calc_a2(diameter){
-  return 0.7 * 5 * diameter;
+  return 0.7 * quantity_value * diameter;
 }
 
 // input forms
@@ -75,25 +75,28 @@ const b_element = document.getElementById("b_dimension");
 const t_element = document.getElementById("t_dimension");
 const r1_element = document.getElementById("r1_screw");
 const d_element = document.getElementById("diameter");
+const alpha_element = document.getElementById("alpha");
+const quantity_element = document.getElementById("quantity");
 const calculate_button = document.getElementById("calculate");
 
 // check if forms are filled
-function areFormsFilled(){
-  if (nd_element.value && alpha_element.value && b_element.value && h_element.value){
-    calculate_button.disabled = false;
-  }
-  else {
-    calculate_button.disabled = true;
-  }
-}
+// function areFormsFilled(){
+//   if (nd_element.value && alpha_element.value && b_element.value && h_element.value){
+//     calculate_button.disabled = false;
+//   }
+//   else {
+//     calculate_button.disabled = true;
+//   }
+// }
 
-nd_element.onkeyup = areFormsFilled;
-r_element.onkeyup = areFormsFilled;
-a_element.onkeyup = areFormsFilled;
-b_element.onkeyup = areFormsFilled;
-t_element.onkeyup = areFormsFilled;
-r1_element.onkeyup = areFormsFilled;
-d_element.onkeyup = areFormsFilled;
+// nd_element.onkeyup = areFormsFilled;
+// r_element.onkeyup = areFormsFilled;
+// a_element.onkeyup = areFormsFilled;
+// b_element.onkeyup = areFormsFilled;
+// t_element.onkeyup = areFormsFilled;
+// r1_element.onkeyup = areFormsFilled;
+// d_element.onkeyup = areFormsFilled;
+// alpha_element.onkeyup = areFormsFilled;
 
 let nd_value = 0;
 let r_value = 0;
@@ -102,6 +105,9 @@ let b_value = 0;
 let t_value = 0;
 let r1_value = 0;
 let d_value = 0;
+let alpha_value = 0;
+let quantity_value = 0;
+let quantity2_value = 0;
 let ym_value = 1.3;
 
 let [aeff_result, rk_result, condition1_result, r1d_result, condition2_result, a3c_result, a3t_result, a4c_result, a4t_result, a1_result, a2_result] = Array(11).fill(0);
@@ -118,7 +124,10 @@ calculate_button.addEventListener('click', () => {
   b_value = parseFloat(b_element.value);
   t_value = parseFloat(t_element.value);
   r1_value = parseFloat(r1_element.value);
-  d_value = parseFloat(t_element.value);
+  d_value = parseFloat(d_element.value);
+  alpha_value = parseFloat(alpha_element.value);
+  quantity_value = parseFloat(quantity.value);
+  quantity2_value = parseFloat(quantity.value * 2);
   
   aeff_result = calc_aeff(a_value, t_value);
   rk_result = calc_rk(aeff_result, r_value, ym_value);
@@ -143,9 +152,9 @@ calculate_button.addEventListener('click', () => {
 
 const latex_aeff_element = document.getElementById("latex_aeff");
 const latex_rk_element = document.getElementById("latex_rk");
-const latex_condition1_element = document.getElementById("latex_condition1");
+const latex_condition1_element = document.getElementById("latex_condition_1");
 const latex_r1d_element = document.getElementById("latex_r1d");
-const latex_condition2_element = document.getElementById("latex_condition2");
+const latex_condition2_element = document.getElementById("latex_condition_2");
 
 const latex_a3c_element = document.getElementById("latex_a3c");
 const latex_a3t_element = document.getElementById("latex_a3t");
@@ -160,27 +169,27 @@ const latex_condition_literal1_element = document.getElementById("latex_conditio
 
 function update_latex() {
   // calculation
-  latex_aeff_element.innerHTML = "$$ A_{eff}={2}*A*t*0.75} = { 2 * {" + a_value + " * " + t_value +"} * 0.75 } = {" + aeff_result.toFixed(2) + "}  $$";
+  latex_aeff_element.innerHTML = "$$ A_{eff}={2}*A*t*0.75 = { 2 * {" + a_value + " * " + t_value +"} * 0.75 } = {" + aeff_result.toFixed(2) + "}  $$";
 
   latex_rk_element.innerHTML = "$$ R_{k}=\\frac{A_{eff}*R}{\\displaystyle{Y_{m}}} = \\frac{" + aeff_result + " * " + r_value +"}{\\displaystyle{" + 1.3 + "} } = {" + rk_result.toFixed(2) + "} $$";
  
   latex_condition1_element.innerHTML = "$$ \\frac{N_{d}}{\\displaystyle{R_{k}}} = \\frac{" + nd_value + " }{\\displaystyle{" + rk_result + "} } = {" + condition1_result.toFixed(2) + "} $$";
  
-  latex_r1d_element.innerHTML = "$$ R_{1,d}=\\frac{ 2 * 5 * R_{1} * k_{mod}}{\\displaystyle{Y_{m}}} = \\frac{ 2 * 5 * " + r1_value + " * " + kmod_value +"}{\\displaystyle{" + 1.3 + "} } = {" + r1d_result.toFixed(2) + "} $$";
+  latex_r1d_element.innerHTML = "$$ R_{1,d}=\\frac{ 2 * {" + quantity_value +"} * R_{1} * k_{mod}}{\\displaystyle{Y_{m}}} = \\frac{ 2 * {" + quantity_value +"} * " + r1_value + " * " + kmod_value +"}{\\displaystyle{" + 1.3 + "} } = {" + r1d_result.toFixed(2) + "} $$";
  
   latex_condition2_element.innerHTML = "$$ \\frac{N_{d}}{\\displaystyle{R_{1,d}}} = \\frac{" + nd_value + " }{\\displaystyle{" + r1d_result + "} } = {" + condition2_result.toFixed(2) + "} $$";
 
-  latex_a3c_element.innerHTML = "$$ a_{3,c}={10 * d} = { 10 * " + d_value + " } = {" + a3c_result.toFixed(2) + "} $$";
+  latex_a3c_element.innerHTML = "$$ a_{3,c}={{" + quantity2_value +"} * d} = { {" + quantity2_value +"} * " + d_value + " } = {" + a3c_result.toFixed(2) + "} $$";
 
-  latex_a3t_element.innerHTML = "$$ a_{3,t}={ 10 + 5 * \\cos(45) * d} = { 10 + 5 * \\cos(45) * {" + d_value +"}} = {" + a3t_result.toFixed(2) + "} $$";
+  latex_a3t_element.innerHTML = "$$ a_{3,t}={ {" + quantity2_value +"} + {" + quantity_value +"} * \\cos({" + alpha_value +"}) * d} = { {" + quantity2_value +"} + {" + quantity_value +"} * \\cos({" + alpha_value +"}) * {" + d_value +"}} = {" + a3t_result.toFixed(2) + "} $$";
 
-  latex_a4c_element.innerHTML = "$$ a_{4,c}={5 * d} = { 5 * " + d_value + " } = {" + a4c_result.toFixed(2) + "} $$";
+  latex_a4c_element.innerHTML = "$$ a_{4,c}={{" + quantity_value +"} * d} = { {" + quantity_value +"} * " + d_value + " } = {" + a4c_result.toFixed(2) + "} $$";
 
-  latex_a4t_element.innerHTML = "$$ a_{4,t}={ 5 + 5 * \\sin(45) * d} = { 10 + 5 * \\sin(45) * {" + d_value +"}} = {" + a4t_result.toFixed(2) + "} $$";
+  latex_a4t_element.innerHTML = "$$ a_{4,t}={ {" + quantity_value +"} + {" + quantity_value +"} * \\sin({" + alpha_value +"}) * d} = { {" + quantity2_value +"} + {" + quantity_value +"} * \\sin({" + alpha_value +"}) * {" + d_value +"}} = {" + a4t_result.toFixed(2) + "} $$";
 
-  latex_a1_element.innerHTML = "$$ a_{1}={ 0.7 * (( 5 + 7 * \\cos(45)) * d} = { 0.7 * (( 5 + 7 * \\cos(45) * {" + d_value +"}} = {" + a1_result.toFixed(2) + "} $$";
+  latex_a1_element.innerHTML = "$$ a_{1}={ 0.7 * (( {" + quantity_value +"} + 7 * \\cos({" + alpha_value +"})) * d} = { 0.7 * (( {" + quantity_value +"} + 7 * \\cos({" + alpha_value +"}) * {" + d_value +"}} = {" + a1_result.toFixed(2) + "} $$";
 
-  latex_a2_element.innerHTML = "$$ a_{2}={0.7 * 5 * d} = { 0.7 * 5 * " + d_value + " } = {" + a2_result.toFixed(2) + "} $$";
+  latex_a2_element.innerHTML = "$$ a_{2}={0.7 * {" + quantity_value +"} * d} = { 0.7 * {" + quantity_value +"} * " + d_value + " } = {" + a2_result.toFixed(2) + "} $$";
   
   latex_condition_literal_element.innerHTML = condition_literal;
 
